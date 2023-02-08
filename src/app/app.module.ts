e1abbe4e9bbf3335c3174cbb047f0331e2291a44
Ainoa Dorado Iglesias <ainoadorado@gmail.com>
2023-02-08 21:44:53 +0100

    Crear componente Blog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 /*componentes*/
 import { AppComponent } from './app.component';
+import { BlogComponent } from './components/blog/blog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
